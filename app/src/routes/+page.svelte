66b5e31ed36ac63827c07fd9734829a6eef0b9a7
therealjohnwall1 <andyvvan@gmail.com>
2024-02-01 16:40:07 -0800

    clean

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,42 +2,43 @@
 	// Images
 	import reload_img from "$lib/imgs/reset.png"
 	import logo_img from "$lib/imgs/winton.jpg"
-	import crown_img from "$lib/imgs/leaderboard_img.png"
-	
-	import Word_body from './word_body.svelte'
+	import crown_img from "$lib/imgs/leaderboard.png"
+
 	
 	import { onMount } from 'svelte';
       // globs
     let WORD_AMT = 25;
     const back_domain = "http://localhost:8000";
-    $: word_bank = [];
-
+    let word_promise;
+	let word_bank;
     console.log("innitalizing word bank");
 
     onMount(async () => {
         console.log(`Mounting dom`)
         console.log(WORD_AMT)
-        word_bank = get_words(WORD_AMT)
-		console.log("type: " + typeof word_bank);
-		console.log("bank: " + word_bank);
+        word_promise = get_words(WORD_AMT)
+		console.log("type: " + typeof word_promise);
+		console.log("bank: " + word_promise);
     });
 
     function handle_keypress(event){
         pass
     }
 
+	//watch promise tutorial on yt
     async function get_words(){
         const params = {
             size: WORD_AMT
         };
 
+		let word_data;
         const queryString = Object.keys(params)
         .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key]))
         .join('&');
        
         const url = `${back_domain}/getWords?${queryString}`;
 
-        await fetch(url)
+        fetch(url)
             .then(response => {
             if (!response.ok) {
             throw new Error(`Error: ${response.status}`);
@@ -46,34 +47,20 @@
             })
             .then(data => {
 			word_bank = data;
-            console.log("works: " + data);
+            console.log("works: " + word_bank);
+			console.log("type: " + typeof word_bank);
             })
             .catch(error => {
             console.error('Error:', error);
             });
     }
 
-	
-	function changeWords15(){
-		WORD_AMT = 15;
+	async function get_words_onclick(){
+		await get_words();
 	}
-	function changeWords25(){
-		WORD_AMT = 25;
+	function testStatus(){
+		console.log(typeof word_bank);
 	}
-	function changeWords50(){
-		WORD_AMT = 50
-	}
-	function changeWords100(){
-		WORD_AMT = 100
-	}
-	function customWords(){
-		pass
-	}
-
-	let count = 1;
-
-	console.log("check");
-	console.log("bank2: " + word_bank);
 
 </script>
 
@@ -86,29 +73,25 @@
 	width = 35/>
 	TypingTest
 	<span>
-		<button>
-			<img src={reload_img} alt="reload img"
-			width = 30/>
-		</button>
-		<!-- <button>
-			<img src={crown_img} alt="button for leaderboards" />
-			height: 10%;
-			width:10%;
-		</button> -->
+	<button on:click = {get_words_onclick}>
+		<img src={reload_img} alt="reload img"
+		width = 30/>
+	</button>
+	<button>
+		<img src={crown_img} alt="button for leaderboards" 
+		width = 35/>
+	</button>
 		
 	</span>
 </h1>
 
-
-<div>
+<!-- <div>
 	{#each word_bank as word}
 		{#each word as letter}
-		<letter> {letter} </letter>
+			<letter> {letter} </letter>
 		{/each}
 	{/each}
-</div>
-
-
+</div> -->
 
 <style>
 	h1{
@@ -117,6 +100,8 @@
 		font-size: 1.75em;
 		font-weight: 600;
 	}
+
+	
 
 	:global(body) { 
 		background-color: rgb(41, 35, 36);
